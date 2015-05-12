e8ff8477f9e0bde8334ce61dfd79f009cf763463
xiaoduoduo <xiaoduoduo@xiaoduoduo-PC>
2015-05-12 22:03:40 +0800

    update

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -4,27 +4,27 @@ app.run(['$rootScope','$http',function($rootScope,$http){
 	$('body').vegas({
 	    timer: false,
 	    transitionDuration: 2000,
-	    slides: [
-	        {
-				src : './img/y002.JPG',
-				fade : 2000
-			}, {
-				src : './img/y005.JPG',
-				fade : 2000
-			}, {
-				src : './img/y008.JPG',
-				fade : 2000
-			}, {
-				src : './img/y010.JPG',
-				fade : 2000
-			}, {
-				src : './img/y019.JPG',
-				fade : 2000
-			}, {
-				src : './img/y041.JPG',
-				fade : 2000
-			}
-	    ],
+//	    slides: [
+//	        {
+//				src : './img/y002.JPG',
+//				fade : 2000
+//			}, {
+//				src : './img/y005.JPG',
+//				fade : 2000
+//			}, {
+//				src : './img/y008.JPG',
+//				fade : 2000
+//			}, {
+//				src : './img/y010.JPG',
+//				fade : 2000
+//			}, {
+//				src : './img/y019.JPG',
+//				fade : 2000
+//			}, {
+//				src : './img/y041.JPG',
+//				fade : 2000
+//			}
+//	    ],
 	    overlay: "styles/images/09.png"
 	});
 	
